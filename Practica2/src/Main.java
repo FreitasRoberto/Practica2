680f7f2899141cf5001118eda5585e2847264a60
Roberto Freitas <freitasroberto04@gmail.com>
2022-05-26 20:11:57 +0200

    Add files via upload

---
+++ Practica2/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int contador = 10;
+        while (contador > 0){
+        System.out.println(contador);
+        contador = contador - 1;
+        }
+    // Mi codigo sigue por aqui
+
+    }
+}
